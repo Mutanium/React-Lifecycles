71cdb3274e21b8d4b606672b33153f7619c014ba
Reinoptland <reinoptland@gmail.com>
2021-06-08 15:46:04 +0200

    Useeffect on mount and on update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
   console.log("WHAT IS COUNT:", count);
 
+  // 2 argumenten:
+  // 1e: callback -> wat wil je doen
+  // 2e: dependency array: -> wanneer wil je dit doen
+
+  useEffect(() => {
+    console.log("LEEG DEPENDENCY ARRAY, MOUNTING");
+  }, []);
+
+  useEffect(() => {
+    console.log("VOER DIT UIT, WANNEER COUNT VERANDERD");
+    if (count >= 10) {
+      setCount(0);
+    }
+  }, [count]);
+
   function addOne() {
-    console.log("VOOR KLIK");
+    // console.log("VOOR KLIK");
     setCount(count + 1); // hierna moet de pagina worden geupdate!
     // Hier heb je nog NIET de volgende state
-    console.log("Na het klikken:", count); // 0
+    // console.log("Na het klikken:", count); // 0
     // Hier na pas:
     // Onder de motorkap: App()
   }
