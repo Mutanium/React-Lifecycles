585eea65e5c1a70857d017ea987c7d4d929d1120
Reinoptland <reinoptland@gmail.com>
2021-06-08 15:31:08 +0200

    State is not updated in event handler yet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [count, setCount] = useState(5);
+  let [count, setCount] = useState(0);
 
   console.log("WHAT IS COUNT:", count);
 
   function addOne() {
+    console.log("VOOR KLIK");
     setCount(count + 1); // hierna moet de pagina worden geupdate!
+    // Hier heb je nog NIET de volgende state
+    console.log("Na het klikken:", count); // 0
+    // Hier na pas:
+    // Onder de motorkap: App()
+  }
+
+  function setTo0() {
+    setCount(0);
+    // na de tweede keer state op nul, wordt de echte dom niet geupadate
   }
 
   return (
@@ -15,6 +25,7 @@ function App() {
       <header className="App-header">
         <p>{count}</p>
         <button onClick={addOne}>+</button>
+        <button onClick={setTo0}>0</button>
       </header>
     </div>
   );
